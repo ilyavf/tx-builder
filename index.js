3dddf8c07a6b2a2d9508060fcedf2461cd822a94
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-07-27 20:55:44 +0300

    added typeforce

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const Buffer = require('safe-buffer').Buffer
 const varuint = require('varuint-bitcoin')
+const typeforce = require('typeforce')
 
 const readSlice = offset => buffer => {
-  return [buffer.slice(0, offset), offset]
+  return [buffer.slice(0, offset), buffer.slice(offset)]
 }
 
-// readUInt32 :: Buffer -> (Res, Offset)
+// readUInt32 :: Buffer -> (Res, Buffer)
 function readUInt32 (buffer) {
+  typeforce(typeforce.Buffer, buffer)
   var i = buffer.readUInt32LE(0)
-  return [i, 4]
+  return [i, buffer.slice(4)]
 }
 
-// readUInt32 :: Buffer -> (Res, Offset)
+// readUInt32 :: Buffer -> (Res, Buffer)
 function readInt32 (buffer) {
+  typeforce(typeforce.Buffer, buffer)
   var i = buffer.readInt32LE(0)
-  return [i, 4]
+  return [i, buffer.slice(4)]
 }
 
 function readUInt64LE (buffer, offset) {
@@ -29,19 +32,24 @@ function readUInt64LE (buffer, offset) {
 
 function readUInt64 (buffer) { // eslint-disable-line no-unused-vars
   var i = readUInt64LE(buffer, 8)
-  return [i, 8]
+  return [i, buffer.slice(8)]
 }
 
+// readVarInt :: Buffer -> (Res, Buffer)
 function readVarInt (buffer) {
   var vi = varuint.decode(buffer, 0)
-  return [vi, varuint.decode.bytes]
+  return [vi, buffer.slice(varuint.decode.bytes)]
 }
 
+// readVarSlice :: Buffer -> (Res, Buffer)
 function readVarSlice (buffer) {
   const [len, offset] = readVarInt(buffer)
   const [res, offset2] = readSlice(len)(buffer.slice(offset))
-  return [res, offset + offset2]
+  return [res, buffer.slice(offset + offset2)]
 }
+
+
+
 
 const txHex = '0100000001545f6161d2be3bdfe7184ee1f72123c3918738da8b97f11e23acdd34059f7a2d010000006b4830450221008c33d765ae16cfa3cc653c5c039d58131fbbdf76266af7a76910fc1ba39de0b8022048ae83fc9b82f62b816641158dd1cfd398d2c56d5f6f812c9fa588947311d8400121033701fc7f242ae2dd63a18753518b6d1425e53496878924b6c0dc08d800af46adffffffff0200e1f505000000001976a91461ca8116d03694952a3ad252d53c695da7d95f6188ac18ddf505000000001976a9145e9f5c8cc17ecaaea1b4e5a3d091ca0aed1342f788ac00000000'
 const buffer = Buffer.from(txHex, 'hex')
@@ -49,6 +57,7 @@ const buffer = Buffer.from(txHex, 'hex')
 // compose :: adds -> (state -> buffer -> [state, buffer])
 function compose (args) {
   return function (state, buffer) {
+    typeforce(typeforce.Buffer, buffer)
     args.reduce(([state, buffer], f) => f(state, buffer), [state, buffer])
     return [state, buffer]
   }
@@ -56,13 +65,39 @@ function compose (args) {
 // add :: propName -> f -> (state -> buffer -> [state, buffer])
 function add (propName, f) {
   return function (state, buffer) {
-    const [res, offset] = f(buffer)
+    typeforce(typeforce.Buffer, buffer)
+    const [res, bufferLeft] = f(buffer)
     state[propName] = res
-    return [state, buffer.slice(offset)]
+    return [state, bufferLeft]
   }
 }
 
-console.log(readInt32(buffer, 0))
+console.log(readInt32(buffer))
+
+// readInputs :: Buffer -> (Res, Offset)
+function readInputs (buffer) {
+  const vins = []
+  let [vinLen, bufferLeft] = readVarInt(buffer)
+  let vin
+  for (let i = 0; i < vinLen; ++i) {
+    [vin, bufferLeft] = readInput(bufferLeft)
+    vins.push(vin)
+  }
+  return [vins, bufferLeft]
+}
+function readOutputs (buffer) {
+
+  // var voutLen = readVarInt()
+  // tx.outs.push({
+  //   value: readUInt64(),
+  //   script: readVarSlice()
+  // })
+
+  const vout = []
+  return [vout, buffer]
+}
+
+
 
 // decodeTx :: buffer -> [vin, buffer]
 const decodeTx = buffer =>
@@ -86,21 +121,11 @@ const readInput = buffer =>
   ])({}, buffer)
 )
 
-// readInputs :: Buffer -> (Res, Offset)
-function readInputs (buffer) {
-  const [vinLen, offset] = readVarInt(buffer)  // eslint-disable-line no-unused-vars
-  const buffer2 = buffer.slice(offset)
-
-  const [vin, buffer3] = readInput(buffer2)
-
-  return [[vin], offset + (buffer2.length - buffer3.length)]
+try {
+  console.log(decodeTx(buffer))
+} catch(e) {
+  console.log(e)
 }
-function readOutputs (buffer) {
-  const vout = []
-  return [vout, 0]
-}
-
-console.log(decodeTx(buffer))
 
 module.exports = {
   readInt32
