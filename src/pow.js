cfb401925598c81229f3362f42420ceb3a26dea8
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-10-20 14:00:51 -0400

    added increment parameter to pow

--- src/pow.js
+++ src/pow.js
@@ -3,20 +3,20 @@ const { bufferUInt64 } = require('./buffer-build')
 const hashFromBuffer = require('./tx-decoder').getTxId
 
 // pow :: Buffer -> Number -> NonceNumber
-const pow = difficulty => (buffer, nonce = 0) => {
+const pow = difficulty => (buffer, nonce = 0, increment = 1) => {
   let hash
-  // let now = Date.now()
+  let now = Date.now()
   const check = Array(difficulty).fill('0').join('')
-  nonce--
-  // console.log(`Starting PoW for ${difficulty} (${check}), initial nonce = ${nonce}:`)
+  nonce -= increment
+  console.log(`Starting PoW for ${difficulty} (${check}), initial nonce = ${nonce}:`)
   do {
-    nonce++
+    nonce += increment
     hash = hashFromBuffer(Buffer.concat([buffer, bufferUInt64(nonce)]))
-    if (nonce % (10 * 1000 * 1000) === 0) {  // (10^7) takes ~168sec
-      // console.log(`- ${Math.floor((Date.now() - now) / 1000)}, nonce = ${nonce}, hash = ${hash}`)
+    if (nonce % (1 * 1000 * 1000) === 0) {  // (10^7) takes ~168sec
+      console.log(`- ${Math.floor((Date.now() - now) / 1000)}, nonce = ${nonce}, hash = ${hash}`)
     }
   } while (hash.slice(0, difficulty) !== check)
-  // console.log(`Finished ${difficulty} with: time = ${Math.floor((Date.now() - now) / 1000)}, nonce = ${nonce}, hash = ${hash}`)
+  console.log(`Finished ${difficulty} with: time = ${Math.floor((Date.now() - now) / 1000)}, nonce = ${nonce}, hash = ${hash}`)
   return nonce
 }
 
--- test/pow.test.js
+++ test/pow.test.js
@@ -6,16 +6,13 @@ describe('proof-of-work', function () {
   describe('pow', function () {
     this.timeout(50 * 3600 * 1000) // 50 minutes
     const buffer = bufferInt32(25)
-    const count = 2
+    const count = 6
     let i
     let nonce = 0
     for (i = 1; i <= count; i++) {
       (function (i) {
         it(`should calculate nonce for difficulty ${i}`, function () {
-          if (i === 7) {
-            nonce = 43190940
-          }
-          nonce = pow(i)(buffer, nonce)
+          nonce = pow(i)(buffer, nonce, 4)
           assert.ok(nonce)
         })
       }(i))
@@ -23,4 +20,6 @@ describe('proof-of-work', function () {
   })
 })
 
-// Finished with: time = 741, nonce = 43190948, hash = 000000019ac673e207a146c5b9bc50961bdbbd6ecfd3d5630403a73fff451bdd
+// Finished for 6 with: time = 3,   nonce = 660344,   hash = 000000d9dcd88ea53e11bf7ecd376ece32a3bf68845ebfee2e92b1ac2e206978
+// Finished 7 / 1 with: time = 741, nonce = 43190948, hash = 000000019ac673e207a146c5b9bc50961bdbbd6ecfd3d5630403a73fff451bdd
+// Finished 7 / 4 with: time = 55, nonce = 43190948, hash = 000000019ac673e207a146c5b9bc50961bdbbd6ecfd3d5630403a73fff451bdd
