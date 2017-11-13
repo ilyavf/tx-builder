4f6816bf3cb926a84e457d77bab999419dd9ee22
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-11-13 12:53:31 -0500

    upgraded packages

--- test/fixtures/hdnode.js
+++ test/fixtures/hdnode.js
@@ -9,6 +9,7 @@ const hdNode = root.derivePath(`m/44'/0'/0'`)
 const addrHdNode = hdNode.derive(0).derive(0)
 const address = addrHdNode.getAddress()
 const xpub = hdNode.neutered().toBase58()
+
 // tpubDCFRCuNqtJHd3htCkN1rtHmQiSsjxNBFcPjqByuQZwqn9yqmNrWNfB4Y72uNFujtmajddf29LwTMDLVfpPXz1LRDXPPo75imk8WNe1ZfbvC
 // console.log(`xpub = ${xpub}`)
 
