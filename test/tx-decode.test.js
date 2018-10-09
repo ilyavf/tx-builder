2f3c1eca114e6801230d2ec3ee7d310d77091bcd
Ilya Fadeev <fadeev.ilya@gmail.com>
2018-10-09 14:40:28 -0400

    added createHash to tx-decoder

--- test/tx-decode.test.js
+++ test/tx-decode.test.js
@@ -1,7 +1,7 @@
 'use strict'
 const Buffer = require('safe-buffer').Buffer
 const assert = require('assert')
-const { decodeTx, readHash, readInput, readOutput, readInputs, getTxId } = require('../src/tx-decoder')
+const { decodeTx, readHash, readInput, readOutput, readInputs, getTxId, createHash } = require('../src/tx-decoder')
 const fixture = require('./fixtures/tx-hex-decoded')
 
 describe('decoder', function () {
@@ -107,6 +107,13 @@ describe('decoder', function () {
     it('should get txid', function () {
       const txBuffer = Buffer.from(fixture.hex, 'hex')
       assert.equal(getTxId({})(txBuffer), fixture.decoded.txid)
+    })
+  })
+
+  describe('createHash', function () {
+    it('should create a hash for buffer data', function () {
+      const buffer = Buffer.from('010203', 'hex')
+      assert.equal(createHash({})(buffer).toString('hex'), '19c6197e2140b9d034fb20b9ac7bb753a41233caf1e1dafda7316a99cef41416')
     })
   })
 })
