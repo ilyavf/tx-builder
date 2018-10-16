4b159652721f62ba8e799b1ec27bbabd79c0ac54
Ilya Fadeev <fadeev.ilya@gmail.com>
2018-10-16 11:05:21 -0400

    utils: added outputScript for P2PKH

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,9 @@
 const bitcoin = require('bitcoinjs-lib')
 const bs58check = require('bs58check')
 const bech32 = require('bech32')
+const OPS = require('bitcoin-ops')
+const typeforce = require('typeforce')
+const types = require('./types')
 
 function getAddress (publicKey, network) {
   network = network || bitcoin.networks.testnet
@@ -21,15 +24,30 @@ function getHexFromBech32Address (address) {
 // Returns hex value of a Bitcoin address:
 // getHexFromAddress :: String -> Buffer
 function getHexFromAddress (address) {
+  typeforce(types.Address, address)
   const payload = bs58check.decode(address)
   const version = payload.readUInt8(0)
   const hash = payload.slice(1)
-  return hash.toString('hex')
+  return hash
+}
+
+// outputScript :: Object -> Buffer
+function outputScript ({ address, hash }) {
+  hash = hash || getHexFromAddress(address)
+  // P2PKH script:
+  return bitcoin.script.compile([
+    OPS.OP_DUP,
+    OPS.OP_HASH160,
+    hash,
+    OPS.OP_EQUALVERIFY,
+    OPS.OP_CHECKSIG
+  ])
 }
 
 module.exports = {
   getAddress,
   getAddressBech32,
   getHexFromBech32Address,
-  getHexFromAddress
+  getHexFromAddress,
+  outputScript
 }
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 const bitcoin = require('bitcoinjs-lib')
-const { getAddress, getAddressBech32, getHexFromAddress } = require('../src/utils')
+const { getAddress, getAddressBech32, getHexFromAddress, outputScript } = require('../src/utils')
 
 describe('Utils', function () {
   const publicKeyHex = '03a6afa2211fc96a4130e767da4a9e802f5e922a151c5cd6d4bffa80358dd1f9a3'
@@ -23,8 +23,16 @@ describe('Utils', function () {
     it('should return hex from Base58Check address', function () {
       const addr = 'msQzKJatdWdw4rpy8sbv8puHoncseekYCf'
       const expected = '8280b37df378db99f66f85c95a783a76ac7a6d59'
-      const hex = getHexFromAddress(addr)
+      const hex = getHexFromAddress(addr).toString('hex')
       assert.equal(hex, expected)
+    })
+  })
+  describe('outputScript', function () {
+    it('should create P2PKH output script off address', function () {
+      const address = 'msQzKJatdWdw4rpy8sbv8puHoncseekYCf'
+      const expected = '76a9148280b37df378db99f66f85c95a783a76ac7a6d5988ac'
+      const script = outputScript({ address })
+      assert.equal(script.toString('hex'), expected)
     })
   })
 })
